e073631fe974278963062f0f23f6f50fe8a54606
klinkewitzn <56318063+klinkewitzn@users.noreply.github.com>
2020-05-28 12:14:19 +0200

    lektion 6

---
+++ Lektion6/Test/Convert.js
@@ -0,0 +1,9 @@
+"use strict";
+var L06Convert;
+(function (L06Convert) {
+    let x = 25;
+    console.log(x);
+    let parsed = parseInt("A7", 16);
+    console.log(parsed);
+})(L06Convert || (L06Convert = {}));
+//# sourceMappingURL=Convert.js.map
---
+++ Lektion6/Test/Convert.ts
@@ -0,0 +1,7 @@
+namespace L06Convert {
+    let x: number = 25; 
+    console.log(x); 
+
+    let parsed: number = parseInt("A7", 16); 
+    console.log(parsed); 
+}
