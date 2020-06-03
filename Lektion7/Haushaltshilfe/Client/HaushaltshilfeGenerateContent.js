0e6c9e80759908d52d8b363627a91fbc316de74c
klinkewitzn <56318063+klinkewitzn@users.noreply.github.com>
2020-06-03 16:56:07 +0200

    l7

---
+++ Lektion7/Haushaltshilfe/Client/HaushaltshilfeGenerateContent.js
@@ -0,0 +1,81 @@
+"use strict";
+//Vorlage= Aufgabe 4 von Tanja Schuchter, da meine Aufgabe4 nicht funktioniert hat
+var A07_Haushaltshilfe;
+(function (A07_Haushaltshilfe) {
+    function generateContent(_data) {
+        for (let category in _data) {
+            let items = _data[category];
+            let group = null;
+            switch (category) {
+                case "Money":
+                    group = createMoney(items, category);
+                    break;
+                case "Household":
+                    group = createHouse(items, category);
+                    break;
+                case "Shopping":
+                    group = createGroceries(items, category);
+                    break;
+                default:
+                    break;
+            }
+            let fieldset = document.querySelector("fieldset#" + category);
+            if (fieldset && group)
+                fieldset.appendChild(group);
+        }
+    }
+    A07_Haushaltshilfe.generateContent = generateContent;
+    function createHouse(_items, _category) {
+        let group = document.createElement("span");
+        for (let item of _items) {
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.setAttribute("price", item.price.toFixed(2));
+            checkbox.value = item.name;
+            checkbox.name = _category;
+            checkbox.id = item.name;
+            let label = document.createElement("label");
+            label.textContent = item.name;
+            label.htmlFor = item.name;
+            group.appendChild(checkbox);
+            group.appendChild(label);
+        }
+        return group;
+    }
+    function createMoney(_items, _category) {
+        let group = document.createElement("span");
+        group.setAttribute("class", "Payment");
+        for (let item of _items) {
+            let radio = document.createElement("input");
+            radio.type = "radio";
+            radio.setAttribute("price", item.price.toFixed(2));
+            radio.value = item.name;
+            radio.name = _category;
+            radio.id = item.name;
+            let label = document.createElement("label");
+            label.textContent = item.name;
+            label.htmlFor = item.name;
+            group.appendChild(radio);
+            group.appendChild(label);
+        }
+        return group;
+    }
+    function createGroceries(_items, _category) {
+        let group = document.createElement("div");
+        for (let item of _items) {
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.setAttribute("price", item.price.toFixed(2));
+            checkbox.value = item.name;
+            checkbox.name = _category;
+            checkbox.id = item.name;
+            let label = document.createElement("label");
+            label.textContent = item.name;
+            label.htmlFor = item.name;
+            group.appendChild(checkbox);
+            group.appendChild(label);
+        }
+        return group;
+    }
+})(A07_Haushaltshilfe || (A07_Haushaltshilfe = {}));
+//# sourceMappingURL=HaushaltshilfeGenerateContent.js.map
---
+++ Lektion7/Haushaltshilfe/Client/haushaltshilfe.js
@@ -0,0 +1,70 @@
+"use strict";
+//Vorlage= Aufgabe 4 von Tanja Schuchter, da meine Aufgabe4 nicht funktioniert hat
+var A07_Haushaltshilfe;
+(function (A07_Haushaltshilfe) {
+    window.addEventListener("load", handleLoad);
+    let form;
+    let url = "http://localhost:5001";
+    async function handleLoad(_event) {
+        let response = await fetch("HaushaltshilfeData.json"); //(await) warten bis fetch die Daten von HouseData.json hat
+        let offer = await response.text(); //text() liefert mir nicht direkt einen string, sondern nur die Promise einen string zu liefern, wenn sie die Daten hat (solage warten ->await)
+        let data = JSON.parse(offer); //JSON.prse wandelt den offer- string in ein Objekt um
+        A07_Haushaltshilfe.generateContent(data);
+        form = document.querySelector("form");
+        let slider = document.querySelector("input#mass");
+        let submitbutton = document.querySelector("button[type=button]");
+        let resetbutton = document.querySelector("button[type=reset]");
+        form.addEventListener("change", handleChange);
+        slider.addEventListener("input", displayMass);
+        resetbutton.addEventListener("click", deleteData);
+        submitbutton.addEventListener("click", sendData);
+        displayOrder();
+    }
+    async function sendData(_event) {
+        console.log("DATA SENT");
+        let formData = new FormData(form);
+        let query = new URLSearchParams(formData);
+        let response = await fetch(url + "?" + query.toString());
+        let responseText = await response.text();
+        alert("" + responseText);
+    }
+    function deleteData() {
+        let order = document.querySelector("div#order");
+        order.innerHTML = "";
+    }
+    function handleChange(_event) {
+        displayOrder();
+    }
+    function displayOrder() {
+        let price = 0;
+        let order = document.querySelector("div#order");
+        order.innerHTML = "";
+        let formData = new FormData(form);
+        for (let entry of formData) {
+            console.log("schleife entry of formData");
+            let item = document.querySelector("[value='" + entry[1] + "']");
+            let itemPrice = Number(item.getAttribute("price"));
+            if (entry[0] == "Household") {
+                order.innerHTML += item.value + "   " + " : € " + itemPrice.toFixed(2) + "<br>";
+            }
+            else if (entry[0] == "Shopping") {
+                order.innerHTML += item.value + "   " + " : € " + itemPrice.toFixed(2) + "<br>";
+            }
+            else if (entry[0] == "Money") {
+                order.innerHTML += item.value + "   " + "<br>";
+            }
+            price += itemPrice;
+        }
+        let progress = document.querySelector("progress");
+        order.innerHTML += "get/submit money" + " : " + progress.value + "€";
+        let gesamt = progress.value + price;
+        console.log(gesamt);
+        order.innerHTML += "<p><strong>Total : " + gesamt + "€";
+    }
+    function displayMass(_event) {
+        let progress = document.querySelector("progress");
+        let amount = _event.target.value;
+        progress.value = parseFloat(amount);
+    }
+})(A07_Haushaltshilfe || (A07_Haushaltshilfe = {}));
+//# sourceMappingURL=haushaltshilfe.js.map
